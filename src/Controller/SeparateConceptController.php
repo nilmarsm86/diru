c9889db699328e8501c637a5708aeec8f315466e
Nilmar Sanchez Muguercia <nilmarsm86@gmail.com>
2025-12-16 10:17:07 -0500

    Fix manage icons

--- src/Controller/SeparateConceptController.php
+++ src/Controller/SeparateConceptController.php
@@ -3,79 +3,86 @@
 namespace App\Controller;
 
 use App\Entity\SeparateConcept;
-use App\Form\SeparateConceptType;
 use App\Repository\SeparateConceptRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\CrudActionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 #[Route('/separate/concept')]
 final class SeparateConceptController extends AbstractController
 {
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route(name: 'app_separate_concept_index', methods: ['GET'])]
-    public function index(SeparateConceptRepository $separateConceptRepository): Response
+    public function index(Request $request, SeparateConceptRepository $separateConceptRepository, CrudActionService $crudActionService): Response
     {
-        return $this->render('separate_concept/index.html.twig', [
-            'separate_concepts' => $separateConceptRepository->findAll(),
-        ]);
+        return $crudActionService->indexAction($request, $separateConceptRepository, 'findSeparateConcepts', 'separate_concept');
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route('/new', name: 'app_separate_concept_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, CrudActionService $crudActionService): Response
     {
         $separateConcept = new SeparateConcept();
-        $form = $this->createForm(SeparateConceptType::class, $separateConcept);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($separateConcept);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_separate_concept_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('separate_concept/new.html.twig', [
-            'separate_concept' => $separateConcept,
-            'form' => $form,
+        return $crudActionService->formLiveComponentAction($request, $separateConcept, 'separate_concept', [
+            'title' => 'Nuevo concepto',
         ]);
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route('/{id}', name: 'app_separate_concept_show', methods: ['GET'])]
-    public function show(SeparateConcept $separateConcept): Response
+    public function show(Request $request, SeparateConcept $separateConcept, CrudActionService $crudActionService): Response
     {
-        return $this->render('separate_concept/show.html.twig', [
-            'separate_concept' => $separateConcept,
-        ]);
+        return $crudActionService->showAction($request, $separateConcept, 'separate_concept', 'separate_concept', 'Detalles del concepto');
     }
 
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
     #[Route('/{id}/edit', name: 'app_separate_concept_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, SeparateConcept $separateConcept, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, CrudActionService $crudActionService, SeparateConcept $separateConcept): Response
     {
-        $form = $this->createForm(SeparateConceptType::class, $separateConcept);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_separate_concept_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('separate_concept/edit.html.twig', [
-            'separate_concept' => $separateConcept,
-            'form' => $form,
+        return $crudActionService->formLiveComponentAction($request, $separateConcept, 'separate_concept', [
+            'title' => 'Editar concepto',
         ]);
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route('/{id}', name: 'app_separate_concept_delete', methods: ['POST'])]
-    public function delete(Request $request, SeparateConcept $separateConcept, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, SeparateConcept $separateConcept, SeparateConceptRepository $separateConceptRepository, CrudActionService $crudActionService): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$separateConcept->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($separateConcept);
-            $entityManager->flush();
+        $successMsg = 'Se ha eliminado el concepto.';
+        $response = $crudActionService->deleteAction($request, $separateConceptRepository, $separateConcept, $successMsg, 'app_separate_concept_index');
+        if ($response instanceof RedirectResponse) {
+            $this->addFlash('success', $successMsg);
+
+            return $response;
         }
 
-        return $this->redirectToRoute('app_separate_concept_index', [], Response::HTTP_SEE_OTHER);
+        return $response;
     }
 }
--- src/Repository/SeparateConceptRepository.php
+++ src/Repository/SeparateConceptRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\SeparateConcept;
+use App\Repository\Traits\PaginateTrait;
+use App\Repository\Traits\SaveData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -12,6 +15,9 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class SeparateConceptRepository extends ServiceEntityRepository implements FilterInterface
 {
+    use SaveData;
+    use PaginateTrait;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, SeparateConcept::class);
@@ -43,6 +49,23 @@ class SeparateConceptRepository extends ServiceEntityRepository implements Filte
     //    }
     public function addFilter(QueryBuilder $builder, string $filter, bool $place = true): void
     {
-        // TODO: Implement addFilter() method.
+        if ('' !== $filter) {
+            $predicate = 'sc.name LIKE :filter ';
+            $builder->andWhere($predicate)
+                ->setParameter(':filter', '%'.$filter.'%');
+        }
+    }
+
+    /**
+     * @return Paginator<mixed>
+     */
+    public function findSeparateConcepts(string $filter = '', int $amountPerPage = 10, int $page = 1): Paginator
+    {
+        $builder = $this->createQueryBuilder('sc')->select(['sc']);
+        $this->addFilter($builder, $filter, false);
+        $query = $builder->addOrderBy('sc.id', 'ASC')
+            ->getQuery();
+
+        return $this->paginate($query, $page, $amountPerPage);
     }
 }
